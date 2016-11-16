a564d81b252e6a95138cb1a2ba12765f23b497df
Hisashi Hata <bism_farm@lowl.eek.jp>
2016-11-16 22:47:04 +0900

    [add] add target for GR-CITRUS
    Add support of GR-CITRUS board

---
+++ asp/target/citrus_gcc/target_device/device_init/DTCa_init.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2015- Hisashi Hata
+ * Released under the toppers license
+ * https://www.toppers.jp/license.html
+ */
+#include "target_board.h"
+#include "driver/rx_gcc/DTCa.h"
+
+#ifdef USE_DTCA_0
+
+void target_dev_ini_dtca_0(){
+  //unlock register access 
+  sil_wrh_mem((void *)(SYSTEM_PRCR_ADDR), SYSTEM_PRKEY | SYSTEM_PRC1);
+  *SYSTEM_MSTPCRA_ADDR &= ~(SYSTEM_MSTPCRA_MSTPA28_DMAC_DTC); /* DTCa */
+  //lock register access
+  sil_wrh_mem((void *)(SYSTEM_PRCR_ADDR), SYSTEM_PRKEY );
+
+  //set dtc vecter base
+  sil_wrw_lem((uint32_t *)DTC_DTCCR, dtca_table);
+}
+
+
+#endif
---
+++ asp/target/citrus_gcc/target_device/use_device.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015- Hisashi Hata       
+ * Released under the toppers license     
+ * https://www.toppers.jp/license.html    
+ */
+
+#ifndef TARGET_USE_DEVICE_H
+#define TARGET_USE_DEVICE_H
+
+//#define USE_RSPI_0
+//#define USE_MMC_RSPI_0
+//#define USE_MMC_GPIO_0
+//#define USE_DTCA_0
+#define USE_SCIC_0
+
+#endif
