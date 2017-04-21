1b83ae38743d394acdcc3194f9f5c11f0cd12327
Hisashi Hata <bism_farm@lowl.eek.jp>
2017-04-21 23:35:54 +0900

    [Fix] Fix pin configuration in mmc_gpio.
    Revert pin configration of mmc_gpio for kaede target.
    Last commit is erroneous.

--- asp/target/kaede_gcc/target_device/device_init/mmc_gpio_init.c
+++ asp/target/kaede_gcc/target_device/device_init/mmc_gpio_init.c
@@ -10,8 +10,8 @@
 #ifdef USE_MMC_GPIO_0
 mmc_gpio_dstat mmc_gpio_st_0 = {
   //card detect io define
-  (uint8_t*)PORTC_PIDR_ADDR,
-  PORT_PIDR_B3_BIT,
+  (uint8_t*)PORTB_PIDR_ADDR,
+  PORT_PIDR_B7_BIT,
   0,  //io voltage is low when card inserted
   //card select io define
   (uint8_t*)PORTC_PODR_ADDR,
@@ -29,11 +29,11 @@ void target_dev_ini_mmc_sdcard_gpio_0(){
   uint8_t rd;
   //config CD pin
   //set input port
-  rd = sil_reb_mem(PORTC_PDR_ADDR);
-  sil_wrb_mem(PORTC_PDR_ADDR, rd & ~PORT_PDR_B3_BIT);
+  rd = sil_reb_mem(PORTB_PDR_ADDR);
+  sil_wrb_mem(PORTB_PDR_ADDR, rd & ~PORT_PDR_B7_BIT);
   //set gpio
-  rd = sil_reb_mem(PORTC_PMR_ADDR);
-  sil_wrb_mem(PORTC_PMR_ADDR, rd & ~PORT_PMR_B3_BIT);
+  rd = sil_reb_mem(PORTB_PMR_ADDR);
+  sil_wrb_mem(PORTB_PMR_ADDR, rd & ~PORT_PMR_B7_BIT);
   //config CS# pin
   //set output port
   rd = sil_reb_mem(PORTC_PDR_ADDR);
